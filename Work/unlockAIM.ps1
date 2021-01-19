7a7c4c24c645cbcf47eb71580ebd4ad0ae63a4a6
BkKSmith <ksmith@oakland.edu>
2021-01-19 14:41:10 -0500

    Update unlockAIM.ps1

--- Work/unlockAIM.ps1
+++ Work/unlockAIM.ps1
@@ -9,14 +9,14 @@ Created By:
 #>
 
 #Get User list
-$userList = Get-ChildItem C:\users\
+$userList = Get-ChildItem C:\users\ | sort LastWriteTime -Descending
 
 #User folder with the most recent file change should be current user
 $currentUser = $userList[0].Name
 
 
 #Search Temp Folder for the current user account for the lock file
-$deleteList = Get-ChildItem -Recurse -Path C:\users\$currentUser\AppData\local\Temp\ -Filter "mmmain.lck" 
+$deleteList = Get-ChildItem -Recurse -Path "C:\users\$currentUser\AppData\local\Temp\" -Filter "mmmain.lck" 
 
 #For each Aim lock file found delete
 foreach($i in $deleteList){
