294244141661241f648d4a419bd7f85eb0ec194a
BkKSmith <vladsimpailer@gmail.com>
2022-06-22 17:39:20 -0400

    Update msiInstaller.ps1

--- Work/ShopEdgeInstaller/msiInstaller.ps1
+++ Work/ShopEdgeInstaller/msiInstaller.ps1
@@ -1,9 +1,9 @@
-#Copy-Item -Path "\\wap01\ShopEdgeDist\Redist\RS Report Viewer version 12" -Recurse -Destination "C:\Temp"
 
 
+
+Copy-Item -Path "\\wap01\ShopEdgeDist\Redist\RS Report Viewer version 12" -Recurse -Destination "C:\Temp"
 Start-Process C:\Windows\System32\msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\SQLSysClrTypes_x64.msi" /quiet /qn /norestart' -Wait
-
-#Start-Process msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\ReportViewer.msi" /quiet /qn /norestart' -Wait
+Start-Process msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\ReportViewer.msi" /quiet /qn /norestart' -Wait
 
 
 #Write-Host "Install complete"
