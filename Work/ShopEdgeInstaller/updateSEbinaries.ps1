9dfa215ecaf49b1a51423154121961d1d9525ccd
BkKSmith <vladsimpailer@gmail.com>
2022-08-12 15:43:33 -0400

    changes to font
    changed for easier visibility and managing

--- Work/ShopEdgeInstaller/updateSEbinaries.ps1
+++ Work/ShopEdgeInstaller/updateSEbinaries.ps1
@@ -11,28 +11,28 @@ $installLog = @()
 
 
     Foreach($i in $computerArray){
-        Write-Host "Attempting to connnect to: $i"
+        Write-Host "Attempting to connnect to: $i" -ForegroundColor Blue
         #Test Connection to host
         $connectionPass = Test-Connection -Quiet $i
         
         If ($connectionPass -eq $True){
-            Write-Host "Connection Completed"
+            Write-Host "Connection Completed" -ForegroundColor Green
             try{
+                Write-Host "Attempting to delete old binaries" -ForegroundColor Blue
                 Remove-Item -Path "\\$i\C$\ShopEdge\*" -Recurse -ErrorAction Stop
-                Write-Host "This should not display"
                 $continueUpdate = $True
             }
             
             catch [System.UnauthorizedAccessException]{
-                "Software is still open"
-                $installLog = $installLog + "$i,Update Failed: Please close the app on this pc"
+                Write-Host "Software is still open" -ForegroundColor Red
+                $installLog = $installLog + "$i,Update Failed: Please close the app on this pc" 
                 $continueUpdate = $False
             }
 
             if ($continueUpdate -eq $True){
-                Write-Host "Old Binaries erased"
+                Write-Host "Old Binaries erased" -ForegroundColor Green
                 Copy-Item -Path "\\wap01\c$\ShopEdgeDist\Binaries\" -Recurse -Destination "\\$i\C$\ShopEdge"
-                Write-Host "New Binaries updated"
+                Write-Host "New Binaries updated" -ForegroundColor Green
                 $installLog = $installLog + "$i,Installation completed"
             }
 
@@ -40,7 +40,7 @@ $installLog = @()
         }
     
         ElseIf($connectionPass -eq $False){
-           Write-Host "The following machine has failed updating: $i"
+           Write-Host "The following machine has failed updating: $i" -ForegroundColor Red
            $installLog = $installLog + "$i,Unable to connect"
         } 
     }
