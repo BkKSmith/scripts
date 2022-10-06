93135962da89d20c69380bc44abcd80f487a35a6
BkKSmith <vladsimpailer@gmail.com>
2022-10-06 13:09:08 -0400

    Updated script
    Changed from local script to a remote script.

--- Work/ShopEdgeInstaller/msiInstaller.ps1
+++ Work/ShopEdgeInstaller/msiInstaller.ps1
@@ -1,17 +1,24 @@
 
+<#
+
+This was created to be able to push a msi install needed for shopedge. Origninal version needed to be ran locally on the computer. This one however, just
+needs the computer to have PSRemoting enabled. Once enabled, then it will run properly. 
+
+Created and owned by: Kyle J Smith II 
+    Updated: 10/6/2022
+    Version: 2.1
+#>
+
 
 #Moved copy over to main installer script to see if the installation is failing at the copy point
 #Copy-Item -Path "\\wap01\ShopEdgeDist\Redist\RS Report Viewer version 12" -Recurse -Destination "C:\Temp"
-Start-Process C:\Windows\System32\msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\SQLSysClrTypes_x64.msi" /quiet /qn /norestart' -Wait -verb runAS
-Start-Process msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\ReportViewer.msi" /quiet /qn /norestart' -Wait -verb runAS
+
+Write-Host "Please enter the computer name:  "
+$targetComputer = Read-Host
+
+Invoke-Command -ComputerName $targetComputer -ScriptBlock{Start-Process C:\Windows\System32\msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\SQLSysClrTypes_x64.msi" /quiet /qn /norestart' -Wait -verb runAS}
+Invoke-Command -ComputerName $targetComputer -ScriptBlock{Start-Process msiexec.exe -ArgumentList '/i "C:\Temp\RS Report Viewer version 12\ReportViewer.msi" /quiet /qn /norestart' -Wait -verb runAS}
 
 
-#Write-Host "Install complete"
 
 
-<#
-
-msiexec.exe /i C:\Temp\SQLSysClrTypes_x64.msi /quiet /qn /norestart /log C:\Temp\installTest.log PROPERTY1=value1 PROPERTY2=value2
-msiexec.exe /i C:\Temp\ReportViewer.msi /quiet /qn /norestart /log C:\Temp\installTest.log
-
-#>
